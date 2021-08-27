45746e30471faf6f53d35ee2fe785e3799ad7729
moritz1896 <moritz.schikora@gmx.de>
2021-08-27 13:33:54 +0200

    finalisierung_1.1

--- app/src/main/java/com/example/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/example/basiccalculator/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.basiccalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric = false
+    var lastDot = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onDigit(view: View){
+        tvInput.append((view as Button). text)
+        lastNumeric = true
+    }
+
+    fun onClear(view: View){
+        tvInput.text  = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot)
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+    }
+
 }
